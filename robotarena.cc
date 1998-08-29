16048426607a33a26de10523e00258fbd1f27264
Ragnar Ouchterlony <ragnar@users.sourceforge.net>
1998-08-29 07:42:42 +0000

    Added timeout function

--- robotarena.cc
+++ robotarena.cc
@@ -11,16 +11,36 @@ extern "C"
 #define DEFAULT_WIDTH  600
 #define DEFAULT_HEIGHT 480
 
+int rakna = 0;
+GTimer klocka;
+double tidssnitt=0;
+bool frsta=false;
+
 void print_help_message( void )
 {
   cout << "   Usage: robotarena [options] robot1 [robot2] [robot3] ... " << endl << endl;
   cout << " Options: No options recognized" << endl << endl;
 }
 
+gint timeout_function(gpointer data)
+{
+  gulong musec = 100;
+  double tid;
+
+  tid = g_timer_elapsed(&klocka, &musec);
+  tidssnitt = tid / ++rakna;
+
+  //  cout << "  Microsecs: " << musec;
+  //  cout << "  Räkna: " << rakna << endl;
+  //  g_timer_reset(&klocka);
+  return true;
+}
+
 int main ( int argc, char * argv[] )
 {
   int i,j;
   int nr_robots,nr_failed=0;
+  gint timeout_tag;
 
   gtk_init (&argc, &argv);
 
@@ -75,7 +95,14 @@ int main ( int argc, char * argv[] )
     
   control_win( nr_robots, robotnames );
   arena_win();
+
+  g_timer_start(&klocka);
+
+  timeout_tag = gtk_timeout_add( 9, GtkFunction(timeout_function), (gpointer) "");
+
   gtk_main();
+
+  cout << "Tidssnitt: " << tidssnitt << "  Räkna: " << rakna << endl;
 
   return EXIT_SUCCESS;
 }
