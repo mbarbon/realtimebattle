1d27f9cf8567ae61fec60cb9f8a1e0a56643ab86
Erik Elmgren <erik@users.sourceforge.net>
1998-08-29 21:02:06 +0000

    Completely redone

--- Arena.cc
+++ Arena.cc
@@ -1,4 +1,15 @@
 #include "Arena.h"
+#include <fcntl.h>
+#include <unistd.h>
+#include <errno.h>
+
+Line::Line()
+{
+  start_point = Vector2D(0.0, 0.0);
+  direction = Vector2D(0.0, 0.0);
+  length = 0.0;
+  thickness = 0.0;
+}
 
 Line::Line(const Vector2D& sp, const Vector2D& d, const double len, const double th)
 {
@@ -6,6 +17,12 @@ Line::Line(const Vector2D& sp, const Vector2D& d, const double len, const double
   direction = d;
   length = len;
   thickness = th;
+}
+
+Circle::Circle()
+{
+  center = Vector2D(0.0, 0.0);
+  radius = 0.0;
 }
 
 Circle::Circle(const Vector2D& c, const double r)
@@ -43,10 +60,71 @@ Circle::get_distance(const Vector2D& pos, const Vector2D& dir, const double size
   return dt - sqrt(c);
 }
 
-Arena::Arena(const char* filename)
+Robot::Robot(char* filename)
+{
+  my_shape = Circle();
+  velocity = Vector2D(0.0, 0.0);
+  robot_filename = *g_string_new(filename);
+  robot_name = *g_string_new("");
+  robot_dir= *g_string_new(getenv("RTB_ROBOTDIR"));
+  extra_air_resistance = 0.0;
+  
+  int pipe_in[2], pipe_out[2];
+  if (pipe (pipe_in))
+    throw Error("Couldn't setup pipe_in for robot ", robot_filename.str, "Robot::Robot");
+  if (pipe (pipe_out))
+    throw Error("Couldn't setup pipe_out for robot ", robot_filename.str, "Robot::Robot");
+
+  if( (robot_pid = fork()) < 0 )
+    throw Error("Couldn't fork childprocess for robot ", robot_filename.str, "Robot::Robot");
+
+  if(robot_pid == 0)   // Child process, to be the new robot
+    {
+      dup2(pipe_out[0],STDIN_FILENO);    // Make pipe_out the standard input for the robot
+      close(pipe_out[1]);
+
+      dup2(pipe_in[1],STDOUT_FILENO);   // Make pipe_in the standard output
+      close(pipe_in[0]);
+
+      // Make the pipes non-blocking
+      int pd_flags;
+      if( (pd_flags = fcntl(pipe_out[0], F_GETFL, 0)) == -1 ) 
+        throw Error("Couldn't get pd_flags for robot ", robot_filename.str, "Robot::Robot, child");
+      pd_flags |= O_NONBLOCK;
+      if( fcntl(pipe_out[0], F_SETFL, pd_flags) == -1 ) 
+        throw Error("Couldn't change pd_flags for robot ", robot_filename.str, "Robot::Robot, child");
+
+      execl(robot_filename.str, robot_filename.str, NULL);  // Should not return!
+      cerr << "Couldn't open robot " << robot_filename.str << "  Error in Robot::Robot" << endl;
+      cerr << "Errno: " << errno << endl;
+      exit(EXIT_FAILURE);
+    }
+  else
+    {
+      close(pipe_out[0]);     // Close input side of pipe_out
+      close(pipe_in[1]);      // Close output side of pipe_in  
+
+      // Make the pipes non-blocking
+      int pd_flags;
+      if( (pd_flags = fcntl(pipe_in[0], F_GETFL, 0)) == -1 ) 
+        throw Error("Couldn't get pd_flags for pipe_in in robot ", robot_filename.str, "Robot::Robot, parent");
+      pd_flags |= O_NONBLOCK;
+      if( fcntl(pipe_in[0], F_SETFL, pd_flags) == -1 ) 
+        throw Error("Couldn't change pd_flags for pipe_in in robot ", robot_filename.str, "Robot::Robot, parent");
+
+      // send_message(MESSAGE_INITIATE);
+    }
+}
+
+Robot::~Robot()
+{
+  //send_message(MESSAGE_EXIT_ROBOT);
+} 
+
+Arena::Arena(char* filename)
 {
   ifstream file(filename);
-  if( !file ) throw FileError();
+  if( !file ) throw Error("Couldn't open file", filename, "Arena::Arena");
 
   parse_file(file);
 }
@@ -123,6 +201,42 @@ Arena::get_shortest_distance(const Vector2D& pos, const Vector2D& dir, const dou
 
   return dist;
 }
+  
+Error::Error(char* strp, char* funcp) 
+{ 
+  error_stringp = g_string_new(strp); 
+  error_string2p = g_string_new(""); 
+  c = '\0'; 
+  function_stringp = g_string_new(funcp); 
+}
 
+Error::Error(char* strp, char* str2p, char* funcp) 
+{ 
+  error_stringp = g_string_new(strp); 
+  error_string2p = g_string_new(str2p); 
+  c = '\0'; 
+  function_stringp = g_string_new(funcp); 
+}
 
+Error::Error(char* strp, char ch, char* funcp) 
+{ 
+  error_stringp = g_string_new(strp); 
+  error_string2p = g_string_new(""); 
+  c = ch; 
+  function_stringp = g_string_new(funcp);
+}
+
+void 
+Error::print_message() 
+{ 
+  if ( c != '\0' )
+    cerr << "Error in " << function_stringp << ": " << error_stringp 
+         << " " << c << endl; 
+  else if (error_string2p != NULL) 
+    cerr << "Error in " << function_stringp << ": " << error_stringp 
+         << " " << error_string2p << endl;
+  else
+    cerr << "Error in " << function_stringp << ": " << error_stringp 
+         << endl;         
+}
 
--- Arena.h
+++ Arena.h
@@ -1,32 +1,79 @@
+#include <glib.h>
 #include <fstream.h>
 #include "Vector2D.h"
+#include <stdlib.h>
+
+#ifndef __ARENA__
+#define __ARENA__
 
 #define min(a,b) ((a) < (b) ? (a) : (b))
 #define max(a,b) ((a) > (b) ? (a) : (b))
 
 static const double infinity = 1.0e10;
 
-// ---------------------  Shape ---------------------
+enum touch_type { NO_ACTION, BOUNCE, TRANSFORM };
+enum object_type { ROBOT, SHOT, WALL, COOKIE, MINE, EXPLOSION };
+static const number_of_object_types = 6;
 
-class Shape 
+// ---------------------  ArenaObject ---------------------
+
+class ArenaObject
 {
 public:
-  virtual double get_distance(const Vector2D& pos, const Vector2D& dir, const double size) = 0;
-  //virtual void get_args(istream&) = 0;
-
-  class ShapeError {};
+  virtual ~ArenaObject() {}
+  virtual object_type get_object_type() = 0;
+  //  class Error;
 };
 
-// ---------------------  Shape : Line ---------------------
+// ---------------------  Explosion : ArenaObject  -------------------
 
-class Line : public Shape 
+class Explosion : public ArenaObject
 {
 public:
-  //Line();
+  Explosion(const Vector2D& c, const double r, const double energy); 
+  ~Explosion() {}
+
+  object_type get_object_type() { return EXPLOSION; }
+private:
+
+};
+
+// ---------------------  SolidObject ---------------------
+
+// class SolidObject : public ArenaObject 
+// {
+// public:
+
+// };
+
+// ---------------------  Shape ---------------------
+
+class Shape : public ArenaObject
+{
+public:
+  virtual ~Shape() {}
+  virtual double get_distance(const Vector2D& pos, const Vector2D& dir, const double size) = 0;
+  //virtual Vector2D get_normal(const Vector2D& pos, const Vector2D& dir, const double size) = 0;
+  //virtual void get_args(istream&) = 0;
+
+private:
+  touch_type touch_action;
+  double bounce_coeff;
+};
+
+
+// ---------------------  Line : Shape ---------------------
+
+class Line : public Shape
+{
+public:
+  Line();
   Line(const Vector2D& sp, const Vector2D& d, const double len, const double th);
   ~Line() {}
 
   double get_distance(const Vector2D& pos, const Vector2D& dir, const double size);
+  Vector2D get_normal(const Vector2D& pos, const Vector2D& dir, const double size);
+  object_type get_object_type() { return WALL; }
 
 private:
   Vector2D start_point;
@@ -35,16 +82,18 @@ private:
   double thickness;
 };
 
-// ---------------------  Shape : Circle ---------------------
+// ---------------------  Circle : Shape ---------------------
 
 class Circle : public Shape 
 {
 public:
-  //Circle();
+  Circle();
   Circle(const Vector2D& c, const double r); 
   ~Circle() {}
 
   double get_distance(const Vector2D& pos, const Vector2D& dir, const double size);
+  Vector2D get_normal(const Vector2D& pos, const Vector2D& dir, const double size);
+  object_type get_object_type() { return WALL; }
   
 private:
   Vector2D center;
@@ -52,22 +101,159 @@ private:
   //  double radiussqr;
 };
 
-// ---------------------  Arena ---------------------
+// ---------------------  Extras : ArenaObject  ---------------------
+
+class Robot;
+
+class Extras : public ArenaObject //, public Circle 
+{
+public:
+  //Extras(const Vector2D& c, const double r); 
+  //~Extras() {}
+  
+  virtual int touch_action(const class Robot&) = 0;
+private:
+  Circle my_shape;
+
+};
+
+// ---------------------  Cookie : Extras  ---------------------
+
+class Cookie : public Extras
+{
+public:
+  Cookie(const Vector2D& c, const double r, const double energy); 
+  ~Cookie() {}
+  
+  int touch_action(const class Robot&);
+  object_type get_object_type() { return COOKIE; }
+private:
+
+};
+
+// ---------------------  Mine : Extras  ---------------------
+
+class Mine : public Extras
+{
+public:
+  Mine(const Vector2D& c, const double r, const double energy); 
+  ~Mine() {}
+
+  int touch_action(const class Robot&);
+  object_type get_object_type() { return MINE; }
+private:
+};
+
+
+// ----------------  MovingObject : ArenaObject  ---------------------
+
+class MovingObject : public ArenaObject//, public Circle 
+{
+public:
+  //MovingObject(const Vector2D& c, const double r); 
+  //~MovingObject() {}
+
+  virtual void move(const double timestep) = 0;
+
+protected:
+  Circle my_shape;
+  Vector2D velocity;
+  
+};
+
+// ---------------------  Robot : MovingObject  ---------------------
+
+class Robot : private MovingObject
+{
+public:
+  Robot(char* filename);
+  Robot(const Vector2D& c, const double r, char* filename); 
+  ~Robot();
+  
+  void move(const double& timestep);  
+  void check_radar(double distance, object_type obj_type);
+  char* get_message(char* readbuffer);
+  void send_message(const char* message);
+
+  object_type get_object_type() { return ROBOT; }
+  char* get_robotname();
+
+private:
+  double extra_air_resistance;
+  GString robot_name;
+  GString robot_filename;
+  GString robot_dir;
+  istream instream;
+  ostream outstream;
+  pid_t robot_pid;  
+};
+
+// ---------------------  Shot : MovingObject  ---------------------
+
+class Shot : private MovingObject
+{
+public:
+  Shot(const Vector2D& c, const double r, const Vector2D& velocity, const double energy); 
+  ~Shot() {}
+
+  void move(const double timestep);
+  object_type get_object_type() { return SHOT; }
+private:
+
+};
+
+// --------------------------  Arena --------------------------
 
 
 class Arena 
 {
 public:
-  Arena(const char*);
+  Arena(char* filename);
   ~Arena();
 
   double get_shortest_distance(const Vector2D& pos, const Vector2D& dir, const double size, int closest_shape);
-
-  class FileError {};
-
+  void update_robots();
+  void move_shots();
+  void update_explosions();
+  void add_to_list(const class ArenaObject&);
+  void remove_from_list(const class ArenaObject&);
+  void add_to_solid_object_list(const class ArenaObject&);
+  void remove_from_solid_object_list(const class ArenaObject&);
+  void set_initial_position_and_direction(const Vector2D&, const Vector2D&);
+  
 private:
   void parse_file(istream&);
 
-  Shape** shapes;
-  int number_of_shapes;
+  GList object_lists[number_of_object_types];
+
+  GList SolidsObjects;
+
+  Shape** shapes;       // To be removed!
+  int number_of_shapes; // -- "" ---
+
+  double air_resistance;
+  double roll_friction;
+  double slide_friction;
 };
+
+class Error
+{
+public:
+  Error() {}
+  Error(char* strp, char* funcp);
+  Error(char* strp, char* str2p, char* funcp);
+  Error(char* strp, char ch, char* funcp);
+  void print_message();
+  
+  friend class Arena;
+  friend class ArenaObject;
+private:
+
+  GString* error_stringp;
+  GString* error_string2p;
+  GString* function_stringp;
+  gchar c;
+};
+
+#endif __ARENA__
+
