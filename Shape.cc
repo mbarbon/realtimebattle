44c571fed600c21d4dc61d947b2b8b2a76302265
Erik Elmgren <erik@users.sourceforge.net>
1998-09-05 18:38:44 +0000

    Implemented within_distance methods

--- Shape.cc
+++ Shape.cc
@@ -1,4 +1,5 @@
 #include "Arena.h"
+#include "math.h"
 
 Line::Line()
 {
@@ -16,6 +17,39 @@ Line::Line(const Vector2D& sp, const Vector2D& d, const double len, const double
   thickness = th;
 }
 
+inline double
+Line::get_distance(const Vector2D& pos, const Vector2D& vel, const double size)
+{
+  Vector2D y = start_point - pos;
+  double v,w,det,s1,s2;
+  det = 1.0/vedge(direction, vel);
+  s1 = ((v=vedge(vel,y)) + (w=((size + thickness) * dot(vel, direction))))*det;
+  s2 = (v-w)*det;
+  if( ( s1 < 0 || s1 > length ) && ( s2 < 0 || s2 > length ) ) return infinity;
+  double t1,t2;
+  s1 < 0.0 || s1 > length ? t1 = infinity : t1 = (vedge(direction, y) + (size+thickness))*det;
+  s2 < 0.0 || s2 > length ? t2 = infinity : t2 = (vedge(direction, y) - (size+thickness))*det;
+  
+  t1 = min(t1,t2);
+  if ( t1 <= 0.0 ) t1 = infinity;
+  return t1;
+}
+
+Vector2D
+Line::get_normal(const Vector2D& pos, const Vector2D& vel, const double size)
+{
+  return Vector2D(0.0, 0.0);  
+}
+
+bool
+Line::within_distance(const Vector2D& pos, const double size)
+{
+  Vector2D y = start_point - pos;
+  
+  return ((fabs(vedge(y, direction)) <= (size+thickness)) &&
+          (-dot(y,direction) >= 0 && -dot(y,direction) <= length));
+}
+
 Circle::Circle()
 {
   center = Vector2D(0.0, 0.0);
@@ -29,31 +63,26 @@ Circle::Circle(const Vector2D& c, const double r)
 }
 
 inline double
-Line::get_distance(const Vector2D& pos, const Vector2D& dir, const double size)
-{
-  Vector2D y = start_point - pos;
-  double v,w,det,s1,s2;
-  det = 1.0/vedge(direction, dir);
-  s1 = ((v=vedge(dir,y)) + (w=((size + thickness) * dot(dir, direction))))*det;
-  s2 = (v-w)*det;
-  if( ( s1 < 0 || s1 > length ) && ( s2 < 0 || s2 > length ) ) return infinity;
-  double t1,t2;
-  s1 < 0.0 || s1 > length ? t1 = infinity : t1 = (vedge(direction, y) + (size+thickness))*det;
-  s2 < 0.0 || s2 > length ? t2 = infinity : t2 = (vedge(direction, y) - (size+thickness))*det;
-  
-  t1 = min(t1,t2);
-  if ( t1 <= 0.0 ) t1 = infinity;
-  return t1;
-}
-
-
-inline double
-Circle::get_distance(const Vector2D& pos, const Vector2D& dir, const double size)
+Circle::get_distance(const Vector2D& pos, const Vector2D& vel, const double size)
 {
   Vector2D y = center - pos;
-  double dt = dot(dir, y);
-  double c = dt*dt + (size+radius)*(size+radius) - lengthsqr(y);
+  double speedsqr = vel[0]*vel[0] + vel[1]*vel[1];
+  double dt = dot(vel, y);
+  double c = dt*dt + speedsqr*((size+radius)*(size+radius) - lengthsqr(y));
   if( c < 0.0 || dt <= 0.0) return infinity;
-  return dt - sqrt(c);
+  return (dt - sqrt(c))/speedsqr;
 }
 
+bool
+Circle::within_distance(const Vector2D& pos, const double size)
+{
+  return ((center[0]-pos[0])*(center[0]-pos[0])+
+          (center[1]-pos[1])*(center[1]-pos[1]) 
+          <= (size+radius)*(size+radius));
+}
+
+Vector2D
+Circle::get_normal(const Vector2D& pos, const Vector2D& vel, const double size)
+{
+  return Vector2D(0.0, 0.0);
+}
