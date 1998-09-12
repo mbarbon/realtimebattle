ddc2bf58c29c1e6f03f32b3831f03451a08ff327
Erik Elmgren <erik@users.sourceforge.net>
1998-09-12 11:16:24 +0000

    Sleeps longer

--- Robots/empty.c
+++ Robots/empty.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+
 //#include "../messagetypes.h"
 
 int main(int argc, char * argv[])
 {
   printf("Name Empty\n");
 
-  sleep(5);
+  sleep(500);
 
   return(EXIT_SUCCESS);
 }
