83957e42233f6a02150946ac4caaa430eb77b874
Ragnar Ouchterlony <ragnar@users.sourceforge.net>
1998-09-27 16:38:45 +0000

    New messages implemented. Works a lot better

--- Robots/thomastest.cc
+++ Robots/thomastest.cc
@@ -3,11 +3,20 @@
 #include <signal.h>
 #include <stdlib.h>
 #include <time.h>
+#include <glib.h>
 #include "../messagetypes.h"
+
+#define abs(x) ((x>0) ? (x) : (-x))
 
 volatile double acceleration = 0.0;
 volatile double robot_rotate = 0.0;
+volatile double cannon_rotate = 0.0, radar_rotate = 0.0;
+double tid, speed, radar_angle, cannon_angle, tid0, slumprotate = 1.0 ;
+double maxspeed = 10.0;
+bool sweep = false, align_cro= true, align_cra = true, enemy = false;
+
 volatile sig_atomic_t exit_robot = false;
+static GTimer* timer;
 
 message_to_robot_type
 name2msg_to_robot_type(char* msg_name)
@@ -29,13 +38,9 @@ check_messages(int sig)
   char text[81];
   message_to_robot_type msg_t;
 
-  srand(time(0));
-
-  //  if(rand() < (RAND_MAX/40))
-  //    {
-  //      robot_rotate = - robot_rotate;
-  //      cout << "Rotate 1 " << robot_rotate << endl;
-  //    }
+  gulong microsecs;
+  g_timer_elapsed(timer, &microsecs);
+  srand(microsecs);
 
   cin.clear();
   while( !cin.eof() )
@@ -51,33 +56,64 @@ check_messages(int sig)
         case GAME_STARTS:
           cout << "Print I am ready" << endl;
           //          cout << "Rotate 1 " << robot_rotate << endl;
-          cout << "Rotate 7 2" << endl;
+          cout << "Rotate 1 2" << endl;
           acceleration = 0.4;
           cout << "Acceleration " << acceleration << endl;
           break;
+        case GAME_FINISHES:
+          cout << "Print The End" << endl;
+          break;
+
+        case SAVE_DATA:
+          cout << "SaveDataFinished" << endl;
+          break;
+
+        case INFO:
+          cin >> tid >> speed  >> cannon_angle;
+          if(rand() < (RAND_MAX/100))
+            {
+              slumprotate = 0.0;
+              while (abs(slumprotate)<0.5)
+                slumprotate = ((double)(rand())/(double)RAND_MAX-0.5)*2.0;
+              // cout << "Print Thomas: slumptal = " << slumprotate << endl;
+            }
+          break;
+          
         case RADAR:
           {
             double dist, energy;
             int object;
 
-            cin >> dist >> object;
+            cin >> dist >> object >> radar_angle;
             switch(object)
               {
               case ROBOT:
-                energy = 20.0/(dist*dist+0.2);
+                enemy = true;
+                energy = 20.0/(dist*dist+0.2)+0.5;
+                robot_rotate = 0.0;
                 if (dist<3.0)
                   {
-                    robot_rotate = 0.0;
-                    acceleration = acceleration/2.0;
+                    if ((speed>1.0)||((dist<1.0)&&(speed>0.01)))
+                      {
+                        cout << "Break 1" << endl;
+                        acceleration = 0.0;
+                      }
+                    else
+                      if (speed<1.0)
+                        {
+                          acceleration = 0.0;
+                        }
                   }
                 else
                   {
-                  acceleration = 2.0;
-                  robot_rotate = 0.0;
+                    if (speed<maxspeed)
+                      acceleration = 2.0;
+                    else
+                      acceleration = 0.0;
                   }
-                cout << "Acceleration " << acceleration << endl;
-                cout << "Rotate 7 " << robot_rotate << endl;
+                cout << "Rotate 1 " << robot_rotate << endl;
                 cout << "Shoot " << energy << endl;
+                cout << "Acceleration " << acceleration << endl;
 
                 //cout << "Print kill" << endl;
                 break;
@@ -85,50 +121,100 @@ check_messages(int sig)
                 {
                   double old_acc = acceleration;
                   double old_rot = robot_rotate;
-
-                  acceleration = old_acc>?0.5;
-                  robot_rotate = old_rot<?0.7;
+                  if (enemy)
+                    {
+                      enemy = false;
+                      slumprotate = -slumprotate;
+                    }
                   if( dist < 1.0 )
                     {
-                    acceleration = -0.5 ;
-                    robot_rotate = 0.0;
+                      if (speed > 0.1)
+                        {
+                          acceleration = 0.0;
+                          robot_rotate = 0.0;
+                          cout << "Break 1" << endl;
+                        }
+                      else
+                        {
+                          robot_rotate = 5.0*slumprotate;
+                          acceleration = 0.1;
+                        }
                     }
                   else
                     {
                       if (dist<4.0)
                         {
-                        acceleration = 0.0 ;
-                        robot_rotate = 3.0 ;
+                          if (speed > 2.0)
+                            {
+                              acceleration = 0.0 ;
+                              robot_rotate = 2.0*slumprotate ;
+                              cout << "Break 0.7" << endl;
+                            }
+                          else
+                            {
+                              acceleration = 0.1;
+                              robot_rotate = 1.0*slumprotate;
+                            }
                         }
+                      else
+                        {
+                          robot_rotate = 0.3*slumprotate;
+                          if (speed<maxspeed)
+                            acceleration = 2.0;
+                          else
+                            acceleration = 0.0;
+                        }
+                        
                     }
+                    cout << "Rotate 1 " << robot_rotate << endl;
                     cout << "Acceleration " << acceleration << endl;
-                    cout << "Rotate 7 " << robot_rotate << endl;
                     
                 }
                 break;
               case SHOT:
-                acceleration = 2.0;
-                robot_rotate = 1.0;
+                if (speed < 1.0)
+                  {
+                    acceleration = 2.0;
+                    robot_rotate = 1.0*slumprotate;
+                  }
+                else
+                  robot_rotate = 3.0*slumprotate;
                 cout << "Acceleration " << acceleration << endl;
-                cout << "Rotate 7 " << robot_rotate << endl;
+                cout << "Rotate 1 " << robot_rotate << endl;
                 //cout << "Print Help me!" << endl;
-                //cout << "Print A shot is approaching!" << endl;
+                //                cout << "Print A shot is approaching!" << endl;
                 break;
               case COOKIE:
-                //cout << "Print Yummy! A cookie" << endl;
+                robot_rotate = 0.0;
+                if (speed<3.0)
+                  acceleration = 2.0;
+                else
+                  acceleration = 0.0;
+                cout << "Rotate 1 " << robot_rotate << endl;
+                cout << "Acceleration " << acceleration << endl;
+                
+                //                cout << "Print Yummy! A cookie" << endl;
                 break;
+
               case MINE:
+                acceleration = 0.0;
+                robot_rotate = 2.0*slumprotate;
+                if (speed > 0.4)
+                  cout << "Break 1" << endl;
+                cout << "Shoot 0.1" << endl;
+                cout << "Acceleration " << acceleration << endl;
+                cout << "Rotate 1 " << robot_rotate << endl;
+                
                 //cout << "Print Avoid this mine" << endl;
                 break;
+
               case EXPLOSION:
                 cout << "Print Avoid! Explosion" << endl;
                 break;
-              default:
-                break;
               }
-            //                cout << "Print " << dist << " " << object << endl;
-          }
           break;
+          }
+
         case COLLISION:
           {
             int tmp;
@@ -144,8 +230,12 @@ check_messages(int sig)
                 acceleration = 2.0;
                 cout << "Acceleration " << acceleration << endl; 
                 break;
-              case MINE: cout << "Print Oh no! A mine!" << endl; break;
-              case COOKIE: cout << "Print Cookie eaten!" << endl; break;
+              case MINE: 
+                //cout << "Print Oh no! A mine!" << endl; 
+                break;
+              case COOKIE: 
+                //cout << "Print Cookie eaten!" << endl; 
+                break;
               case WALL:
                 acceleration = 0.0;
                 cout << "Acceleration " << acceleration << endl; 
@@ -167,15 +257,17 @@ check_messages(int sig)
           break;
         }
     }
-  acceleration = acceleration*1.001<?2.0;
   signal (sig, check_messages);
 }
 
 int 
 main(int argc, char * argv[])
 {
-  robot_rotate = 0.53;
+//robot_rotate = 0.53;
 
+  timer = g_timer_new();
+  g_timer_reset(timer);
+  srand(time(0));
   check_messages(SIGUSR1);
 
   for(;;sleep(1))
