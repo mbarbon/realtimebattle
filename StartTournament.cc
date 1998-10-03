165e2c7a92a15a6d70d92ae6cdac2ae5494e233a
Ragnar Ouchterlony <ragnar@users.sourceforge.net>
1998-10-03 17:48:52 +0000

    Added buttons and Arenaselecting

--- StartTournament.cc
+++ StartTournament.cc
@@ -31,6 +31,8 @@ Gui::setup_start_tournament_window()
   gtk_container_add (GTK_CONTAINER (start_tournament_window), vbox);
   gtk_widget_show (vbox);
 
+  // Widgets for selecting robots
+
   hbox = gtk_hbox_new (FALSE, 5);
   gtk_container_add (GTK_CONTAINER (vbox), hbox);
   gtk_widget_show (hbox);
@@ -47,57 +49,33 @@ Gui::setup_start_tournament_window()
   gtk_box_pack_start (GTK_BOX (hbox), robots_in_tournament_clist, TRUE, TRUE, 0);
   gtk_widget_show( robots_in_tournament_clist );
 
-  GtkWidget * middle_vbox = gtk_vbox_new (FALSE, 5);
-  gtk_container_add (GTK_CONTAINER (hbox), middle_vbox);
-  gtk_widget_show (middle_vbox);
+  GtkWidget * vbox2 = gtk_vbox_new (FALSE, 5);
+  gtk_box_pack_start( GTK_BOX(hbox), vbox2, TRUE, TRUE, 0);
+  gtk_widget_show (vbox2);
 
-  gtk_widget_show( start_tournament_window );
+  {
+    char * button_labels[] = { "Remove", "Select All", "Unselect All" };
+    for(int i=0;i<3;i++)
+      {
+        GtkWidget * button = gtk_button_new_with_label( button_labels[i] );
+        gtk_box_pack_start( GTK_BOX(vbox2), button, TRUE, FALSE, 0);
+        gtk_widget_show( button );
+      }
+  }
 
-  const char * arrow_xpms[2][11] = {
-    {"13 8 2 1",
-     "       c None",
-     "x      c #000000000000",
-     "   xx        ",
-     "  xx         ",
-     " xx          ",
-     "xxxxxxxxxxxxx",
-     "xxxxxxxxxxxxx",
-     " xx          ",
-     "  xx         ",
-     "   xx        "},
+  vbox2 = gtk_vbox_new (FALSE, 5);
+  gtk_box_pack_start( GTK_BOX(hbox), vbox2, TRUE, TRUE, 0);
+  gtk_widget_show (vbox2);
 
-    {"13 8 2 1",
-     "       c None",
-     "x      c #000000000000",
-     "        xx   ",
-     "         xx  ",
-     "          xx ",
-     "xxxxxxxxxxxxx",
-     "xxxxxxxxxxxxx",
-     "          xx ",
-     "         xx  ",
-     "        xx   "}};
-
-  for(int i=0;i<2;i++)
-    {
-      GdkPixmap * pixmap;
-      GdkBitmap * bitmap_mask;
-      pixmap = gdk_pixmap_create_from_xpm_d( start_tournament_window->window, &bitmap_mask,
-                                             the_arena->get_background_colour_p(),
-                                             (gchar **)arrow_xpms[i] );
-      GtkWidget * pixmap_widget = gtk_pixmap_new( pixmap, bitmap_mask );
-      gtk_widget_show( pixmap_widget );
-      GtkWidget * button = gtk_button_new();
-      gtk_container_add( GTK_CONTAINER(button), pixmap_widget );
-
-      //  button_info_t * data;
-      //  data = new button_info_t( this, (stat_button_t)(i + 3) );
-      //  gtk_signal_connect (GTK_OBJECT (button), "clicked",
-      //                      GTK_SIGNAL_FUNC (buttons_in_statistics_callback), (gpointer) data);
-      //  gtk_widget_set_usize(button, 28,14);
-      gtk_box_pack_start (GTK_BOX (middle_vbox), button, TRUE, FALSE, 0);
-      gtk_widget_show (button);
-    }
+  {
+    char * button_labels[] = { "Add", "Select All", "Unselect All" };
+    for(int i=0;i<3;i++)
+      {
+        GtkWidget * button = gtk_button_new_with_label( button_labels[i] );
+        gtk_box_pack_start( GTK_BOX(vbox2), button, TRUE, FALSE, 0);
+        gtk_widget_show( button );
+      }
+  }
 
   char * rob_dir_title[1] = { "Robots in Directory" };
   robots_in_directory_clist = gtk_clist_new_with_titles(1, rob_dir_title);
@@ -112,30 +90,122 @@ Gui::setup_start_tournament_window()
   gtk_widget_show( robots_in_directory_clist );
 
   DIR * robotdir;
-  if( NULL != (robotdir = opendir(getenv("RTB_ROBOTDIR"))))
-    {
-      struct dirent * entry;
-      while (NULL != ( entry = readdir( robotdir ) ) )
-        {
-          GString * full_file_name;
-          full_file_name = g_string_new(getenv("RTB_ROBOTDIR"));
-          g_string_append( full_file_name, entry->d_name );
-          struct stat filestat;
-          if( 0 == stat( full_file_name->str, &filestat ) )
-            if( S_ISREG( filestat.st_mode) && (filestat.st_mode & S_IXOTH) ) // Check if file is a regular file that can be executed
-              {     
-                char * list[] = { "" };
+  if( NULL != getenv("RTB_ROBOTDIR"))
+    if( NULL != (robotdir = opendir(getenv("RTB_ROBOTDIR"))))
+      {
+        struct dirent * entry;
+        while (NULL != ( entry = readdir( robotdir ) ) )
+          {
+            GString * full_file_name;
+            full_file_name = g_string_new(getenv("RTB_ROBOTDIR"));
+            g_string_append( full_file_name, entry->d_name );
+            struct stat filestat;
+            if( 0 == stat( full_file_name->str, &filestat ) )
+              if( S_ISREG( filestat.st_mode) && (filestat.st_mode & S_IXOTH) ) // Check if file is a regular file that can be executed
+                {     
+                  char * list[] = { "" };
           
-                int row = gtk_clist_append(GTK_CLIST(robots_in_directory_clist), list);
-                gtk_clist_set_foreground(GTK_CLIST(robots_in_directory_clist), row, the_arena->get_foreground_colour_p());
-                gtk_clist_set_background(GTK_CLIST(robots_in_directory_clist), row, the_arena->get_background_colour_p());
+                  int row = gtk_clist_append(GTK_CLIST(robots_in_directory_clist), list);
+                  gtk_clist_set_foreground(GTK_CLIST(robots_in_directory_clist), row, the_arena->get_foreground_colour_p());
+                  gtk_clist_set_background(GTK_CLIST(robots_in_directory_clist), row, the_arena->get_background_colour_p());
           
-                gtk_clist_set_text(GTK_CLIST(robots_in_directory_clist), row, 0, entry->d_name);
-              }
-        }
-      closedir( robotdir );
-    }
+                  gtk_clist_set_text(GTK_CLIST(robots_in_directory_clist), row, 0, entry->d_name);
+                }
+          }
+        closedir( robotdir );
+      }
 
+  // Widgets for selecting arenas
+
+  hbox = gtk_hbox_new (FALSE, 5);
+  gtk_container_add (GTK_CONTAINER (vbox), hbox);
+  gtk_widget_show (hbox);
+
+  char * arena_tour_title[1] = { "Arenas in Tournament" };
+  arenas_in_tournament_clist = gtk_clist_new_with_titles(1, arena_tour_title);
+  gtk_clist_set_selection_mode (GTK_CLIST(arenas_in_tournament_clist), GTK_SELECTION_MULTIPLE);
+  gtk_clist_set_border(GTK_CLIST(arenas_in_tournament_clist), GTK_SHADOW_IN);
+  gtk_clist_set_column_width(GTK_CLIST(arenas_in_tournament_clist), 0, 90);
+  gtk_clist_set_column_justification(GTK_CLIST(arenas_in_tournament_clist), 0, GTK_JUSTIFY_LEFT);
+  gtk_clist_set_policy(GTK_CLIST(arenas_in_tournament_clist), GTK_POLICY_AUTOMATIC,
+                       GTK_POLICY_AUTOMATIC);
+  gtk_widget_set_usize(arenas_in_tournament_clist, 150, 150);
+  gtk_box_pack_start (GTK_BOX (hbox), arenas_in_tournament_clist, TRUE, TRUE, 0);
+  gtk_widget_show( arenas_in_tournament_clist );
+
+  vbox2 = gtk_vbox_new (FALSE, 5);
+  gtk_box_pack_start( GTK_BOX(hbox), vbox2, TRUE, TRUE, 0);
+  gtk_widget_show (vbox2);
+
+  {
+    char * button_labels[] = { "Remove", "Select All", "Unselect All" };
+    for(int i=0;i<3;i++)
+      {
+        GtkWidget * button = gtk_button_new_with_label( button_labels[i] );
+        gtk_box_pack_start( GTK_BOX(vbox2), button, TRUE, FALSE, 0);
+        gtk_widget_show( button );
+      }
+  }
+
+  vbox2 = gtk_vbox_new (FALSE, 5);
+  gtk_box_pack_start( GTK_BOX(hbox), vbox2, TRUE, TRUE, 0);
+  gtk_widget_show (vbox2);
+
+  {
+    char * button_labels[] = { "Add", "Select All", "Unselect All" };
+    for(int i=0;i<3;i++)
+      {
+        GtkWidget * button = gtk_button_new_with_label( button_labels[i] );
+        gtk_box_pack_start( GTK_BOX(vbox2), button, TRUE, FALSE, 0);
+        gtk_widget_show( button );
+      }
+  }
+
+  char * arena_dir_title[1] = { "Arenas in Directory" };
+  arenas_in_directory_clist = gtk_clist_new_with_titles(1, arena_dir_title);
+  gtk_clist_set_selection_mode (GTK_CLIST(arenas_in_directory_clist), GTK_SELECTION_MULTIPLE);
+  gtk_clist_set_border(GTK_CLIST(arenas_in_directory_clist), GTK_SHADOW_IN);
+  gtk_clist_set_column_width(GTK_CLIST(arenas_in_directory_clist), 0, 90);
+  gtk_clist_set_column_justification(GTK_CLIST(arenas_in_directory_clist), 0, GTK_JUSTIFY_LEFT);
+  gtk_clist_set_policy(GTK_CLIST(arenas_in_directory_clist), GTK_POLICY_AUTOMATIC,
+                       GTK_POLICY_AUTOMATIC);
+  gtk_widget_set_usize(arenas_in_directory_clist, 150, 150);
+  gtk_box_pack_start (GTK_BOX (hbox), arenas_in_directory_clist, TRUE, TRUE, 0);
+  gtk_widget_show( arenas_in_directory_clist );
+
+  cout << "Open arenadir" << endl;
+
+  DIR * arenadir;
+  if( NULL != getenv("RTB_ARENADIR"))
+    if( NULL != (arenadir = opendir(getenv("RTB_ARENADIR"))))
+      {
+        cout << "Dir opened" << endl;
+        struct dirent * entry;
+        while (NULL != ( entry = readdir( arenadir ) ) )
+          {
+            GString * full_file_name;
+            full_file_name = g_string_new(getenv("RTB_ARENADIR"));
+            g_string_append( full_file_name, entry->d_name );
+            struct stat filestat;
+            if( 0 == stat( full_file_name->str, &filestat ) && full_file_name->len > 6 )
+              // Check if file is a regular file that is readable and ends with .arena
+              if( S_ISREG( filestat.st_mode) &&
+                  (filestat.st_mode & S_IROTH)  &&
+                  strcmp(".arena",&(full_file_name->str[full_file_name->len - 6])) == 0 )
+                {     
+                  char * list[] = { "" };
+          
+                  int row = gtk_clist_append(GTK_CLIST(arenas_in_directory_clist), list);
+                  gtk_clist_set_foreground(GTK_CLIST(arenas_in_directory_clist), row, the_arena->get_foreground_colour_p());
+                  gtk_clist_set_background(GTK_CLIST(arenas_in_directory_clist), row, the_arena->get_background_colour_p());
+          
+                  gtk_clist_set_text(GTK_CLIST(arenas_in_directory_clist), row, 0, entry->d_name);
+                }
+          }
+        closedir( arenadir );
+      }
+
+  gtk_widget_show( start_tournament_window );
   start_tournament_up = true;
 }
 
