4b25721c7c6a0f7e74eff23467264937ebd9e104
Erik Elmgren <erik@users.sourceforge.net>
1998-10-08 17:37:42 +0000

    Added some new functions and some other changes

--- Various.cc
+++ Various.cc
@@ -1,15 +1,71 @@
-long
+#include <math.h>
+#include <gdk/gdk.h>
+#include <stdlib.h>
+
+#include "Various.h"
+#include "Error.h"
+
+int
 factorial(const int n)
 {
-  long fact = 1;
+  double fact = 1.0;
   for(int i=1; i<=n; i++)  
     fact *= i;
 
-  return fact;
+  if( fact < (double)INT_MAX )
+    return (int)fact;
+  else
+    return INT_MAX;
 }
 
-long
+int
 binomial(const int n, const int k)
 {
-  return factorial(n) / ( factorial(k) * factorial(n-k) );
+  int k2 = min(k, n-k);
+  
+  double bin = 1;
+  for(int i=n; i>=n-k2; i--)  
+    bin *= i;
+
+  for(int i=1; i<=k2; i++)  
+    bin /= i;
+
+  if( bin < (double)INT_MAX )
+    return (int)bin;
+  else
+    return INT_MAX;
+}
+
+GdkColor
+make_gdk_colour(const long col)
+{
+  GdkColormap *cmap;
+  GdkColor colour;
+
+  cmap = gdk_colormap_get_system();
+  colour.red =   ((col & 0xff0000) >> 16 ) * 0x101;
+  colour.green = ((col & 0x00ff00) >> 8  ) * 0x101;
+  colour.blue =  (col & 0x0000ff) * 0x101;
+  if( !gdk_color_alloc (cmap, &colour) )
+    throw Error("Couldn't allocate colour", "Arena::make_gdk_color");
+
+  return colour;
+}
+
+void 
+reorder_pointer_array(void** array, int size)
+{
+  int n1, n2;
+  void* temp_p;
+  for(int i=0; i<size*5; i++)
+    {
+      n1 = (int)floor(size*((double)rand() / (double)RAND_MAX)) ;
+      n2 = (int)floor(size*((double)rand() / (double)RAND_MAX)) ;
+      if( n1 != n2 )
+        {
+          temp_p = array[n1];
+          array[n1] = array[n2];
+          array[n2] = temp_p;
+        }
+    }
 }
--- Various.h
+++ Various.h
@@ -2,11 +2,20 @@
 #ifndef __VARIOUS__
 #define __VARIOUS__
 
+#include <gdk/gdktypes.h>
 
-long factorial(const int n);
+#define min(a,b) ((a) < (b) ? (a) : (b))
+#define max(a,b) ((a) > (b) ? (a) : (b))
+//#define abs(a) ((a) > 0 ? (a) : -(a))
+//#define sgn(a) ((a) > 0 ? 1 : -1)
 
-long binomial(const int n, const int k);
+
+int factorial(const int n);
+
+int binomial(const int n, const int k);
 
 GdkColor make_gdk_colour(const long col);
+
+void reorder_pointer_array(void** array, int size);
 
 #endif __VARIOUS__
