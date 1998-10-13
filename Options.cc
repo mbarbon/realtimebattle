06425ace81e82e2bc1c7224e9a9ebde59cddb3a9
Erik Elmgren <erik@users.sourceforge.net>
1998-10-13 21:06:28 +0000

    Changed option values to the new structure

--- Options.cc
+++ Options.cc
@@ -12,56 +12,164 @@ extern class Gui the_gui;
 
 Options::Options()
 {
-  all_double_options[OPTION_GRAV_CONST] = new option_info_t<double>(ENTRY_DOUBLE,9.82,0.2,20.0,12,"Gravitioninal Constant", NULL);
-
+  all_double_options[OPTION_GRAV_CONST] = 
+    option_info_t<double>(ENTRY_DOUBLE, 9.82, 0.2, 20.0, 12,"Gravitational Constant", NULL);
   grav_const = 9.82;
+
+  all_double_options[OPTION_AIR_RESISTANCE] = 
+    option_info_t<double>(ENTRY_DOUBLE, 0.005, 0.0, 1.0, 12,"Air resistance", NULL);
   air_resistance = 0.005;
+
+  all_double_options[OPTION_ROLL_FRICTION] = 
+    option_info_t<double>(ENTRY_DOUBLE, 0.002, 0.0, 1.0, 12,"Roll friction", NULL);
   roll_friction = 0.002;
+
+  all_double_options[OPTION_SLIDE_FRICTION] = 
+    option_info_t<double>(ENTRY_DOUBLE, 0.1, 0.0, 5.0, 12,"Slide/break friction", NULL);
   slide_friction = 0.1;
+
+  all_double_options[OPTION_MAX_ACCELERATION] = 
+    option_info_t<double>(ENTRY_DOUBLE, 2.0, 0.1, 10.0, 12,"Max acceleration", NULL);
   max_acceleration = 2.0;
+
+  all_double_options[OPTION_MIN_ACCELERATION] = 
+    option_info_t<double>(ENTRY_DOUBLE, -0.5, -10.0, 0.0, 12,"Min acceleration", NULL);
   min_acceleration = -0.5;
     
+  all_double_options[OPTION_ROBOT_RADIUS] = 
+    option_info_t<double>(ENTRY_DOUBLE, 0.5, 0.1, 10.0, 12, "Robot radius", NULL);
   robot_radius = 0.5;
+
+  all_double_options[OPTION_ROBOT_MASS] = 
+    option_info_t<double>(ENTRY_DOUBLE, 1.0, 0.01, 100.0, 12, "Robot mass", NULL);
   robot_mass = 1.0;
+
+  all_double_options[OPTION_ROBOT_BOUNCE_COEFF] = 
+    option_info_t<double>(ENTRY_DOUBLE, 0.7, 0.0, 1.0, 12, "Robot bounce coefficient", NULL);
   robot_bounce_coeff = 0.7;
+
+  all_double_options[OPTION_ROBOT_HARDNESS] = 
+    option_info_t<double>(ENTRY_DOUBLE, 0.5, 0.0, 1.0, 12, "Robot hardness coefficient", NULL);
   robot_hardness = 0.5;
+
+  all_double_options[OPTION_ROBOT_PROTECTION] = 
+    option_info_t<double>(ENTRY_DOUBLE, 0.5, 0.0, 1.0, 12, "Robot protection coefficient", NULL);
   robot_protection = 0.5;
 
+  all_double_options[OPTION_ROBOT_START_ENERGY] = 
+    option_info_t<double>(ENTRY_DOUBLE, 100.0, 0.01, 10000.0, 12, "Robot start energy", NULL);
   robot_start_energy = 100.0;
+
+  all_double_options[OPTION_ROBOT_MAX_ENERGY] = 
+    option_info_t<double>(ENTRY_DOUBLE, 120.0, 0.01, 10000.0, 12, "Robot max energy", NULL);
   robot_max_energy = 120.0;
 
-  robot_front_protection = 0.9;
-  robot_front_hardness = 0.9;
+  all_double_options[OPTION_ROBOT_FRONT_BOUNCE_COEFF] = 
+    option_info_t<double>(ENTRY_DOUBLE, 0.7, 0.0, 1.0, 12, "Robot front bounce coefficient", NULL);
   robot_front_bounce_coeff = 0.7;
+
+  all_double_options[OPTION_ROBOT_FRONT_HARDNESS] = 
+    option_info_t<double>(ENTRY_DOUBLE, 0.9, 0.0, 1.0, 12, "Robot front hardness coefficient", NULL);
+  robot_front_hardness = 0.9;
+
+  all_double_options[OPTION_ROBOT_FRONT_PROTECTION] = 
+    option_info_t<double>(ENTRY_DOUBLE, 0.9, 0.0, 1.0, 12, "Robot front protection coefficient", NULL);
+  robot_front_protection = 0.9;
+
+  all_double_options[OPTION_ROBOT_COS_FRONTSIZE] = 
+    option_info_t<double>(ENTRY_DOUBLE, cos(M_PI/6.0), -1.0, 1.0, 12, "Robot frontsize (cos(front angle))", NULL);
   robot_cos_frontsize = cos(M_PI/6.0);   // 2*30 degrees
 
+  all_double_options[OPTION_ROBOT_MAX_ROTATE] = 
+    option_info_t<double>(ENTRY_DOUBLE, 2.0*M_PI / 8.0, 2.0*M_PI / 50.0, 2.0*M_PI * 5.0, 12, "Robot max rotate speed [rad/s]", NULL);
   robot_max_rotate = 2.0*M_PI / 8.0;   // [rad / s]
+
+  all_double_options[OPTION_ROBOT_CANNON_MAX_ROTATE] = 
+    option_info_t<double>(ENTRY_DOUBLE, 2.0*M_PI / 4.0, 2.0*M_PI / 50.0, 2.0*M_PI * 5.0, 12, "Robot cannon max rotate speed [rad/s]", NULL);
   robot_cannon_max_rotate = 2.0*M_PI / 4.0;   // [rad / s]
+
+  all_double_options[OPTION_ROBOT_RADAR_MAX_ROTATE] = 
+    option_info_t<double>(ENTRY_DOUBLE, 2.0*M_PI / 3.0, 2.0*M_PI / 50.0, 2.0*M_PI * 5.0, 12, "Robot radar max rotate speed [rad/s]", NULL);
   robot_radar_max_rotate = 2.0*M_PI / 3.0;   // [rad / s]
     
+  all_double_options[OPTION_SHOT_RADIUS] = 
+    option_info_t<double>(ENTRY_DOUBLE, 0.1, 0.01, 1.0, 12, "Shot radius", NULL);
   shot_radius = 0.1;
+
+  all_double_options[OPTION_SHOT_SPEED] = 
+    option_info_t<double>(ENTRY_DOUBLE, 10.0, 0.1, 100.0, 12, "Shot speed", NULL);
   shot_speed = 10.0;
+
+  all_double_options[OPTION_SHOOTING_PENALTY] = 
+    option_info_t<double>(ENTRY_DOUBLE, 0.075, 0.0, 1.0, 12, "Shooting penalty (times shot energy)", NULL);
   shooting_penalty = 0.075;
+
+  all_double_options[OPTION_SHOT_MIN_ENERGY] = 
+    option_info_t<double>(ENTRY_DOUBLE, 0.5, 0.0, 100.0, 12, "Shot min energy", NULL);
   shot_min_energy = 0.5;
+
+  all_double_options[OPTION_SHOT_MAX_ENERGY] = 
+    option_info_t<double>(ENTRY_DOUBLE, 30.0, 0.0, 100000000.0, 12, "Shot max energy", NULL);
   shot_max_energy = 30.0;
+
+  all_double_options[OPTION_SHOT_ENERGY_INCREASE_SPEED] = 
+    option_info_t<double>(ENTRY_DOUBLE, 10.0, 0.0, 100000000.0, 12, "Shot energy increase speed [energypoints/s]", NULL);
   shot_energy_increase_speed = 10.0;   // [energypoints / s]
 
+  all_long_options[OPTION_BACKGROUND_COLOUR] = 
+    option_info_t<long>(ENTRY_HEX, 0xfaf0e6, 0x000000, 0xffffff, 6, "Background colour", NULL);
   background_colour = 0xfaf0e6;  // linen
+
+  all_long_options[OPTION_FOREGROUND_COLOUR] = 
+    option_info_t<long>(ENTRY_HEX, 0x000000, 0x000000, 0xffffff, 6, "Foreground colour", NULL);
   foreground_colour = 0x000000;   // black
 
+  all_double_options[OPTION_COOKIE_MAX_ENERGY] = 
+    option_info_t<double>(ENTRY_DOUBLE, 15.0, 0.0, 100000000.0, 12, "Cookie max energy", NULL);
   cookie_max_energy = 15.0;
+
+  all_double_options[OPTION_COOKIE_MIN_ENERGY] = 
+    option_info_t<double>(ENTRY_DOUBLE, 10.0, 0.0, 100000000.0, 12, "Cookie min energy", NULL);
   cookie_min_energy = 10.0;
+
+  all_double_options[OPTION_COOKIE_FREQUENCY] = 
+    option_info_t<double>(ENTRY_DOUBLE, 1.0/30.0, 0.0, 100000000.0, 12, "Cookie frequency [cookies per second]", NULL);
   cookie_frequency = 1.0/30.0;
+
+  all_double_options[OPTION_COOKIE_RADIUS] = 
+    option_info_t<double>(ENTRY_DOUBLE, 0.3, 0.01, 1.0, 12, "Cookie radius", NULL);
   cookie_radius = 0.3;
+
+  all_long_options[OPTION_COOKIE_COLOUR] = 
+    option_info_t<long>(ENTRY_HEX, 0x35d715, 0x000000, 0xffffff, 6, "Cookie colour", NULL);
   cookie_colour = 0x35d715;
 
+  all_double_options[OPTION_MINE_MAX_ENERGY] = 
+    option_info_t<double>(ENTRY_DOUBLE, 25.0, 0.0, 100000000.0, 12, "Mine max energy", NULL);
   mine_max_energy = 25.0;
+
+  all_double_options[OPTION_MINE_MIN_ENERGY] = 
+    option_info_t<double>(ENTRY_DOUBLE, 15.0, 0.0, 100000000.0, 12, "Mine min energy", NULL);
   mine_min_energy = 15.0;
+
+  all_double_options[OPTION_MINE_FREQUENCY] = 
+    option_info_t<double>(ENTRY_DOUBLE, 1.0/15.0, 0.0, 100000000.0, 12, "Mine frequency [mines per second]", NULL);
   mine_frequency = 1.0/15.0;
+
+  all_double_options[OPTION_MINE_RADIUS] = 
+    option_info_t<double>(ENTRY_DOUBLE, 0.3, 0.01, 1.0, 12, "Mine radius", NULL);
   mine_radius = 0.3;
+
+  all_long_options[OPTION_MINE_COLOUR] = 
+    option_info_t<long>(ENTRY_HEX, 0xff0000, 0x000000, 0xffffff, 6, "Mine colour", NULL);
   mine_colour = 0xff0000;
 
+  all_double_options[OPTION_MAX_TIMESTEP] = 
+    option_info_t<double>(ENTRY_DOUBLE, 0.5, 0.05, 1.0, 12, "Max timestep", NULL);
   max_timestep = 0.5;
+
+  all_double_options[OPTION_TIMEOUT] = 
+    option_info_t<double>(ENTRY_DOUBLE, 120.0, 1.0, 100000000.0, 12, "Timeout [s]", NULL);
   timeout = 120.0;        // i.e. 2 minutes
 
   options_window_up = false;
@@ -69,10 +177,10 @@ Options::Options()
 
 Options::~Options()
 {
-  for(int i=0;i<LAST_DOUBLE_OPTION;i++)
-    delete all_double_options[i];
-  for(int i=0;i<LAST_LONG_OPTION;i++)
-    delete all_long_options[i];
+  //  for(int i=0;i<LAST_DOUBLE_OPTION;i++)
+  //    delete all_double_options[i];
+  //  for(int i=0;i<LAST_LONG_OPTION;i++)
+  //    delete all_long_options[i];
 
   // No char or bool options yet
   //  for(int i=0;i<LAST_STRING_OPTION;i++)
