98d2ce805adf16be040cb3b24ade0c92a67666c2
Erik Elmgren <erik@users.sourceforge.net>
1998-10-24 17:08:57 +0000

    Bugfix

--- Shape.cc
+++ Shape.cc
@@ -70,7 +70,7 @@ Line::get_distance(const Vector2D& pos, const Vector2D& vel, const double size)
   if( s < 0 || s > length ) return infinity;
   if( t < 0 && ((d=-dot(y, direction)) < 0.0 || d > length ) ) return infinity;
 
-  return t;
+  return max(t, 0);
 }
 
 Vector2D
@@ -135,7 +135,7 @@ Circle::get_distance(const Vector2D& pos, const Vector2D& vel, const double size
   double r = size+radius;
   double c = dt*dt + speedsqr * (r*r - lengthsqr(y));
   if( c < 0.0 || dt <= 0.0) return infinity;
-  return (dt - sqrt(c))/speedsqr;
+  return max( (dt - sqrt(c))/speedsqr, 0);
 }
 
 bool
@@ -188,9 +188,10 @@ InnerCircle::get_distance(const Vector2D& pos, const Vector2D& vel, const double
 {
   Vector2D y = center - pos;
   double speedsqr = lengthsqr(vel);
+  if( speedsqr == 0.0 ) return infinity;
   double dt = dot(vel, y);
   double c = dt*dt + speedsqr*((size-radius)*(size-radius) - lengthsqr(y));
-  return (dt + sqrt(c))/speedsqr;
+  return max( (dt + sqrt(c))/speedsqr, 0 );
 }
 
 bool
