4d8b31b24662a016d2da080bae142aa96fea6da7
Erik Elmgren <erik@users.sourceforge.net>
1998-12-29 21:53:57 +0000

    Changed to c-style comment

--- Robots/empty/empty.c
+++ Robots/empty/empty.c
@@ -21,7 +21,7 @@ Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
 #include <stdlib.h>
 #include <unistd.h>
 
-//#include "../messagetypes.h"
+/*#include "../messagetypes.h"*/
 
 int main(int argc, char * argv[])
 {
