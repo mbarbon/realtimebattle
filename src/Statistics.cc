2c224d205c9dedd8acdfb625900446e54d151575
Erik Elmgren <erik@users.sourceforge.net>
1999-01-27 22:15:49 +0000

    Removed 'string.h'

--- src/Statistics.cc
+++ src/Statistics.cc
@@ -20,7 +20,7 @@ Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
 #include <iostream.h>
 #include <iomanip.h>
 #include <math.h>
-#include <string.h>
+//#include <string.h>
 #include <stdlib.h>
 
 #include "Gui.h"
