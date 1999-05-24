0990c8dba2528d5157a954ce5aefca677189c7be
Ragnar Ouchterlony <ragnar@users.sourceforge.net>
1999-05-24 16:15:33 +0000

    Added ArenaController.h as include-file

--- src/ArenaReplay.cc
+++ src/ArenaReplay.cc
@@ -23,6 +23,7 @@ Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
 #endif
 
 #include "ArenaReplay.h"
+#include "ArenaController.h"
 #include "Various.h"
 #include "Options.h"
 #include "Extras.h"
