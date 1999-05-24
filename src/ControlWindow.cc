41646b3e2d737ff2689d84b12ffd30e5f74953d2
Ragnar Ouchterlony <ragnar@users.sourceforge.net>
1999-05-24 17:41:08 +0000

    Removed old searching for games and sequences, will be replaced by new routines

--- src/ControlWindow.cc
+++ src/ControlWindow.cc
@@ -593,56 +593,24 @@ void
 ControlWindow::next_game( GtkWidget* widget,
                           class ControlWindow* cw_p )
 {
-  replay_arena.search_forward( String( "G" ) );
 }
 
 void
 ControlWindow::prev_game( GtkWidget* widget,
                           class ControlWindow* cw_p )
 {
-  replay_arena.search_backwards( String( "G" ) );
-  if( replay_arena.get_current_replay_time() < 2.0 &&
-      the_arena.get_game_nr() + the_arena.get_sequence_nr() > 2 )
-    replay_arena.search_backwards( String( "G" ) );
 }
 
 void
 ControlWindow::next_seq( GtkWidget* widget,
                          class ControlWindow* cw_p )
 {
-  int seq = the_arena.get_sequence_nr();
-  int sit = the_arena.get_sequences_in_tournament();
-
-  if( sit <= 1 || seq == sit )
-    return;
-
-  int new_seq = seq;
-  
-  while( new_seq == seq )
-    new_seq = str2int( get_segment
-                       ( replay_arena.search_forward( String( "G" ) ), 1, -1 ) );
 }
 
 void
 ControlWindow::prev_seq( GtkWidget* widget,
                          class ControlWindow* cw_p )
 {
-  int seq = the_arena.get_sequence_nr();
-  int sit = the_arena.get_sequences_in_tournament();
-  int game = the_arena.get_game_nr();
-
-  if( sit <= 1 || seq == 1 )
-    return;
-
-  int new_game = game;
-  int new_seq = seq;
-
-  while( new_game != 1 || ( game == 1 && new_seq == seq ) )
-    {
-      String result = replay_arena.search_backwards( String( "G" ) );
-      new_seq = str2int( get_segment( result, 1, -1 ) );
-      new_game = str2int( get_segment( result, result.find( ' ' ), -1 ) );
-    }
 }
 
 void
