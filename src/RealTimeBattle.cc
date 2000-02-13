dfa006a1666fedb1d141f4b191bb64d76dea455d
Ragnar Ouchterlony <ragnar@users.sourceforge.net>
2000-02-13 15:20:34 +0000

    Moved lot of function into ArenaController.

--- src/RealTimeBattle.cc
+++ src/RealTimeBattle.cc
@@ -80,42 +80,6 @@ class ArenaController the_arena_controller;
 
 bool no_graphics;
 
-void 
-print_help_message()
-{
-  cout << endl;
-  cout << _(" Usage: RealTimeBattle [options] ") << endl << endl;
-  cout << _(" Options:") << endl;
-  cout << _("    --debug_mode,                -d   debug mode") << endl;
-  cout << _("    --debug_level [0-5],         -D   sets the initial debug level. Implies -d") << endl;
-  cout << _("    --normal_mode,               -n   normal mode (default)") << endl;
-  cout << _("    --competition_mode,          -c   competition mode") << endl ;
-  cout << endl;
-  cout << _("    --no_graphics,               -g   no graphics will be displayed") << endl ;
-  cout << _("    --option_file [file],        -o   selects option-file (default: $HOME/.rtbrc)")  << endl;
-  cout << endl;
-  cout << _("    --log_file [file],           -l   make log file, if 'file' is '-'\n"
-            "                                      the log is sent to STDOUT") << endl;
-  cout << endl;
-  cout << _("    --tournament_file [file],    -t   specify a tournament file to\n"
-            "                                      autostart a tournament") << endl;
-  cout << endl;
-  cout << _("    --statistics_file [file],    -s   file to print the statistics to\n"
-            "                                       when autostarting") << endl;
-  cout << endl;
-  cout << _("    --message_file [file],       -m   redirect messages to 'file'.\n"
-            "                                      With '-' as 'file', STDOUT is used.\n"
-            "                                      If both log and messages are sent\n"
-            "                                      to STDOUT, '-m' will be ignored") << endl;
-  cout << endl;
-  cout << _("    --replay [file]              -r   a log file to replay.\n"
-            "                                      if '-' is specified as file,\n"
-            "                                      input is taken from STDIN") << endl;
-  cout << endl;
-  cout << _("    --help,                      -h   prints this message") << endl;
-  cout << _("    --version,                   -v   prints the version number") << endl;
-  cout << endl;
-}
 
 void
 update_function(const long int interval_usec)
@@ -165,198 +129,6 @@ sigfpe_handler(int signum)
   signal(signum, sigfpe_handler);
 }
 
-void
-parse_command_line(int argc, char **argv)
-{
-  int version_flag=false, help_flag=false, graphics_flag=true;
-  int c;
-
-  extern char* optarg;
-  extern int optind;
-
-  static struct option long_options[] =
-  {
-    //option, argument?, flag, value
-    {"version", 0, &version_flag, true},
-    {"help", 0, &help_flag, true},
-
-    {"debug_mode", 0, &the_arena_controller.game_mode,
-     ArenaBase::DEBUG_MODE},
-    {"debug_level", 1, 0, 0},
-    {"normal_mode", 0, &the_arena_controller.game_mode,
-     ArenaBase::NORMAL_MODE},
-    {"competition_mode", 0, &the_arena_controller.game_mode,
-     ArenaBase::COMPETITION_MODE},
-    {"option_file", 1, 0, 0},
-    {"log_file", 1, 0, 0},
-    {"statistics_file", 1, 0, 0},
-    {"tournament_file", 1, 0, 0},
-    {"message_file", 1, 0, 0},
-    {"replay", 1, 0, 0},
-
-    {"no_graphics", 0, &graphics_flag, false},
-
-    {0, 0, 0, 0}
-  };
-
-  for(;;)
-    {
-      int option_index = 0;
-     
-      c = getopt_long( argc, argv, "dncD:vho:l:s:t:m:r:g",
-                       long_options, &option_index );
-
-      // Detect the end of the options.
-      if (c == -1)
-        break;
-     
-      switch (c)
-        {
-
-        case 0:
-          // If this option set a flag, do nothing else now.
-          // This is not the case for debug_mode
-          if( long_options[option_index].flag != 0 &&
-              option_index != 2 )
-            break;
-          
-          switch( option_index )
-            {
-            case 3:
-              the_arena_controller.debug_level = str2int( optarg );
-              the_arena_controller.game_mode = ArenaBase::DEBUG_MODE;
-              break;
-            case 6: 
-              the_arena_controller.option_filename = (String)optarg;
-              break;
-            case 7:
-              the_arena_controller.log_filename = (String)optarg;
-              break;
-            case 8:
-              the_arena_controller.statistics_filename = (String)optarg;
-              break;
-            case 9:
-              the_arena_controller.tournament_filename = (String)optarg;
-              the_arena_controller.auto_start_and_end = true;
-              break;
-            case 10:
-              the_arena_controller.message_filename = (String)optarg;
-              break;
-            case 11:
-              the_arena_controller.replay_filename = (String)optarg;
-              break;
-            default:
-              Error( true, "Bad error, this shouldn't happen",
-                     "RealTimeBattle.cc:parse_command_line" );
-              exit( EXIT_FAILURE );
-            }
-          break;
-
-
-        case 'd':
-          the_arena_controller.game_mode = ArenaBase::DEBUG_MODE;
-          break;
-
-        case 'D':
-          the_arena_controller.debug_level = str2int( optarg );
-          the_arena_controller.game_mode = ArenaBase::DEBUG_MODE;
-          break;
-
-        case 'n':
-          the_arena_controller.game_mode = ArenaBase::NORMAL_MODE;
-          break;
-
-        case 'c':
-          the_arena_controller.game_mode = ArenaBase::COMPETITION_MODE;
-          break;
-
-        case 'v':
-          version_flag = true;
-          break;
-
-        case 'h':
-          help_flag = true;
-          break;
-
-        case 'o':
-          the_arena_controller.option_filename = (String)optarg;
-          break;
-
-        case 'l':
-          the_arena_controller.log_filename = (String)optarg;
-          break;
-
-        case 's':
-          the_arena_controller.statistics_filename = (String)optarg;
-          break;
-
-        case 't':
-          the_arena_controller.tournament_filename = (String)optarg;
-          the_arena_controller.auto_start_and_end = true;
-          break;
-
-        case 'm':
-          the_arena_controller.message_filename = (String)optarg;
-          break;
-
-        case 'r':
-          the_arena_controller.replay_filename = (String)optarg;
-          break;
-
-        case 'g':
-          graphics_flag = false;
-          break;
-
-        default:
-          print_help_message();
-          exit( EXIT_FAILURE );
-        }
-    }
-
-  if( the_arena_controller.debug_level > max_debug_level )
-    the_arena_controller.debug_level = 5;
-  if( the_arena_controller.debug_level < 0 )
-    the_arena_controller.debug_level = 0;
-
-  if(optind != argc) 
-    {
-      print_help_message();
-      exit( EXIT_FAILURE );
-    }
-
-  if( version_flag )
-      cout << "RealTimeBattle " << VERSION << endl;
-
-  if( help_flag )
-    print_help_message();
-
-  if( help_flag || version_flag ) exit( EXIT_SUCCESS );
-
-  if( the_arena_controller.option_filename == "" )
-    {
-      the_opts.get_options_from_rtbrc();
-      the_arena_controller.option_filename = ".rtbrc";
-    }
-  else
-    the_opts.read_options_file( the_arena_controller.option_filename,true );
-
-  the_arena_controller.auto_start_and_end
-    = ( ( the_arena_controller.tournament_filename != "" ) ||
-        ( the_arena_controller.replay_filename == "-" ) );
-
-  no_graphics = !graphics_flag;
-}
-
-#ifndef NO_GRAPHICS
-void*
-GIMain_temp( void* arg )
-{
-  int res = GIMain( (ArenaController*) arg );
-  // Change this!!!!!
-  return NULL;
-}
-#endif
-
 int 
 main ( int argc, char* argv[] )
 {
@@ -372,22 +144,14 @@ main ( int argc, char* argv[] )
   bindtextdomain( "RealTimeBattle", RTB_LOCALEDIR );
   textdomain( "RealTimeBattle" );
 
-  parse_command_line(argc, argv);
+  the_arena_controller.parse_command_line( argc, argv );
 
-  if( the_arena_controller.tournament_filename != "" )
-    the_arena_controller.start_realtime_arena();
-  else if( the_arena_controller.replay_filename != "" )
-    the_arena_controller.start_replay_arena();
+  //  parse_command_line(argc, argv);
+
 
   signal(SIGCHLD, sig_handler);
   signal(SIGPIPE, sig_handler);
   signal(SIGFPE, sigfpe_handler);
-
-#ifndef NO_GRAPHICS
-  GIInit( argc, argv );
-  pthread_t gui_thread;
-  pthread_create(&gui_thread, NULL, GIMain_temp, (void*)&the_arena_controller);
-#endif
 
   update_function( (long int)(the_opts.get_d( OPTION_UPDATE_INTERVAL ) * 1000000.0) );
   
