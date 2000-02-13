7a1c3b7d648e9948532991b0f01edec05c566153
Ragnar Ouchterlony <ragnar@users.sourceforge.net>
2000-02-13 18:39:57 +0000

    Added a GuiInterface class

--- src/GuiInterface.h
+++ src/GuiInterface.h
@@ -21,9 +21,19 @@ Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
 #define __GUIINTERFACE__
 
 #include <pthread.h>
+#include <list>
 
-class String;
+#include "Options.h"
+#include "ArenaBase.h"
+
 class ArenaController;
+class String;
+class Vector2D;
+
+
+// ---------------------------------------------------------------------------
+// These functions should be defined by the gui.
+// ---------------------------------------------------------------------------
 
 // The name of the gui should be returned as a const String
 const String GIName();
@@ -34,9 +44,83 @@ const String GIUsageMessage();
 bool GIInit( int , char** );
 // The main loop of the gui
 int  GIMain( ArenaController* );
-// Exit from gui
+// Exit from gui. This function is not needed to be set from gui.
 void GIExit( int );
 
 
+// ---------------------------------------------------------------------------
+// class GuiInterface
+// ---------------------------------------------------------------------------
+// All interactions between the gui and server should go through this class.
+// This class needs to be threadsafe!
+// ---------------------------------------------------------------------------
+
+class GuiInterface
+{
+public:
+  struct message_t
+  {
+    message_t( const class String m, const class String f ) : message(m), from(f) {}
+    class String message;
+    class String from;
+  };
+
+  // Functions that supplies information to the Gui.
+
+  // The lists shouldn't be supplied directly 
+  List<Shape>* get_object_lists               (); 
+  List<Robot>* get_all_robots_in_sequence     ();
+  List<Robot>* get_all_robots_in_tournament   ();
+  String get_current_arena_filename           ();
+  int get_game_nr                             ();
+  int get_games_per_sequence                  ();
+  int get_sequence_nr                         ();
+  int get_sequences_in_tournament             ();
+  int get_robots_per_game                     ();
+  int get_number_of_robots                    ();
+  double get_total_time                       ();
+  state_t get_state                           ();
+  Vector2D* get_boundary                      ();
+  enum ArenaBase::game_mode_t get_game_mode   ();
+  int get_debug_level                         ();
+  int get_max_debug_level                     ();
+
+  const double   get_opt_d                    ( option_double_t option );
+  const long int get_opt_l                    ( option_long_t   option );
+  const String   get_opt_s                    ( option_string_t option );
+
+  // Functions that enables the gui to have some influence over the server.
+
+  message_t get_message                       ();
+  int set_debug_level                         ( const int new_level );
+  void pause_game_toggle                      ();
+  void step_paused_game                       ();
+
+  void interrupt_tournament                   ();
+  void end_game                               ();
+
+  void start_tournament    ( const List<start_tournament_info_t>& robotfilename_list, 
+                             const List<start_tournament_info_t>& arenafilename_list, 
+                             const int robots_p_game, 
+                             const int games_p_sequence, 
+                             const int n_o_sequences );
+
+  void set_opt_d                              ( option_double_t option, double   val );
+  void set_opt_l                              ( option_long_t   option, long int val );
+  void set_opt_s                              ( option_string_t option, String   val );
+
+  // Functions for the server.
+
+  void add_message                            ( String message, String from );
+  //TODO: Perhaps an eventlist?
+
+
+private:
+
+  list<message_t> messages;
+
+  pthread_t thread;
+  // other pthread info
+};
 
 #endif __GUIINTERFACE__
