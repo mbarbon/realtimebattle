8cccdc169494670f451c866abaa4d1b3aede3227
Erik Ouchterlony <ejlo@users.sourceforge.net>
2000-02-18 18:15:42 +0000

    Added EventHandler and Event class

---
+++ src/EventHandler.cc
@@ -0,0 +1,95 @@
+/*
+RealTimeBattle, a robot programming game for Unix
+Copyright (C) 1998-2000  Erik Ouchterlony and Ragnar Ouchterlony
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software Foundation,
+Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+*/
+
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif
+
+#include <queue>
+
+
+#include "EventHandler.h"
+#include "Event.h"
+#include "Timer.h"
+#include "Various.h"
+
+
+EventHandler::EventHandler()
+{
+  finished = false;
+}
+
+EventHandler::~EventHandler()
+{
+}
+
+
+
+
+void
+EventHandler::main_loop()
+{
+
+  do
+    {
+
+      if( event_queue.empty() )
+        Error(true, "Event queue unexpectedly empty", "EventHandler::eval_next_event");
+
+      double time_for_next_event;
+  
+      struct timeval time_to_wait;
+  
+      for(;;) 
+        {
+          time_for_next_event = event_queue.top().get_time();
+          //          timer.double2timeval( time_for_next_event, time_to_wait );
+
+          if( time_to_wait.tv_usec == 0 ) break;    // Time to eval next event
+          
+          select(FD_SETSIZE, NULL, NULL, NULL, &time_to_wait);
+        }
+
+      current_time = timer.get();
+
+      event_queue.top().eval();
+
+      event_queue.pop();
+  
+    }  while( !finished );
+
+}
+
+//
+// Call this function with
+//
+// insert_event( new FooEvent( args ) );
+//
+// The event is deleted after evaluation.
+void 
+EventHandler::insert_event( Event* ev )
+{
+  event_queue.push( *ev );
+}
+
+void
+EventHandler::quit()
+{
+  insert_event( new QuitEvent( get_time(), this ) );
+}
---
+++ src/EventHandler.h
@@ -0,0 +1,73 @@
+/*
+RealTimeBattle, a robot programming game for Unix
+Copyright (C) 1998-2000  Erik Ouchterlony and Ragnar Ouchterlony
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software Foundation,
+Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+*/
+
+#ifndef __EVENT_HANDLER__
+#define __EVENT_HANDLER__
+
+#include <queue>
+
+#include "Event.h"
+#include "Timer.h"
+
+class QuitEvent;
+
+
+class EventHandler
+{
+
+public:
+
+  EventHandler();
+  ~EventHandler();
+
+  void main_loop();
+
+  void insert_event( Event* ev );
+
+  double get_time() { return current_time; }
+
+  void quit();
+private:
+  
+  friend class QuitEvent;
+
+  void finish() { finished = true; }
+
+  priority_queue<class Event> event_queue;
+  
+  Timer timer;
+  double current_time;
+
+  bool finished;
+};
+
+class QuitEvent : public Event
+{
+public:
+  QuitEvent( const double time, EventHandler* ev) 
+    : my_event_handler(ev), Event(time) {}
+
+  void eval() const { my_event_handler->finish(); }
+
+protected:
+  EventHandler* my_event_handler;
+};
+
+
+#endif __EVENT_HANDLER__
