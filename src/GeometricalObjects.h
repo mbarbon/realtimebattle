d0a18627616b506dccbd535a62936fba02ac6aed
Erik Ouchterlony <ejlo@users.sourceforge.net>
2000-03-16 22:35:58 +0000

    Minor changes

--- src/GeometricalObjects.h
+++ src/GeometricalObjects.h
@@ -23,8 +23,6 @@ Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
 #include "Shape.h"
 #include "Vector2D.h"
 
-//class Vector2D;
-
 // ---------------------  Line : Shape ---------------------
 
 class Line : public Shape
