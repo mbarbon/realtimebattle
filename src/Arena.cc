51b0f6d4299e002742aff4167345cbb9e914f536
Ragnar Ouchterlony <ragnar@users.sourceforge.net>
2000-04-28 17:37:33 +0000

    Added some work on Define and EndDefine

--- src/Arena.cc
+++ src/Arena.cc
@@ -22,8 +22,11 @@ Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
 
 #include "Arena.h"
 #include "Gadget.h"
+#include "WeaponGadget.h"
 #include "Vector2D.h"
 #include "String.h"
+#include "Messagetypes.h"
+#include "Various.h"
 
 void
 Arena::load_arena_file( const string& filename, Gadget& hierarchy )
@@ -33,6 +36,8 @@ Arena::load_arena_file( const string& filename, Gadget& hierarchy )
 
   stack<ifstream*> file_stack;
   file_stack.push( new ifstream( filename.c_str() ) );
+
+  Gadget* current_gadget = &hierarchy;
 
   while( !file_stack.empty() )
     {
@@ -52,12 +57,60 @@ Arena::load_arena_file( const string& filename, Gadget& hierarchy )
             file_stack.push( new ifstream( wordlist[1].c_str() ) );
           else if( wordlist[0] == "Define" && wordlist.size() > 2 )
             {
+              int i = 0;
+              for( ;i < LAST_GADGET; i++ )
+                if( equal_strings_nocase( wordlist[1], gadget_types[i] ) )
+                  break;
+              switch( i )
+                {
+                case GAD_WEAPON:
+                  {
+                    // TODO: Remember to delete this somewehere!!!!!!!!!
+                    WeaponGadget* gadget =
+                      new WeaponGadget( wordlist[2].c_str(), current_gadget );
+                    current_gadget->my_gadgets.add( gadget->get_info() );
+                    current_gadget = gadget;
+                    break;
+                  }
+                case GAD_SHOT: 
+                  break;
+                case GAD_EXPLOSION: 
+                  break;
+                case GAD_DEFENSE: 
+                  break;
+                case GAD_ENGINE: 
+                  break;
+                case GAD_SENSOR: 
+                  break;
+                case GAD_COMMUNICATION: 
+                  break;
+                case GAD_WALL: 
+                  break;
+                case GAD_DEVICE: 
+                  break;
+                case GAD_BALL: 
+                  break;
+                case GAD_EQUIPMENT: 
+                  break;
+                case GAD_ENVIRONMENT: 
+                  break;
+                case GAD_ROBOTBODY: 
+                  break;
+                case GAD_SCORING: 
+                  break;
+                case GAD_ACTION: 
+                  break;
+                case GAD_VARIABLE: 
+                  break;
+                }
               // TODO: Create new Gadget in the hierarchy, might be:
               // initialize_gadget( wordlist[1], wordlist[2] );
             }
           else if( wordlist[0] == "EndDefine" )
             {
-              // TODO: Gadgetdefinition finished
+              if( wordlist.size() > 2 && wordlist[2] != current_gadget->get_name() )
+                Error( true, "Ending wrong definition", "Arena::load_arena_file" );
+              current_gadget = current_gadget->parent;
             }
           else
             {
