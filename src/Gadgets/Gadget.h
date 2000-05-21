a7cdd2dc7bb007211aa7ae6b71cf257828d2b84b
Ragnar Ouchterlony <ragnar@users.sourceforge.net>
2000-05-21 11:32:19 +0000

    Moved to its own directory called

---
+++ src/Gadgets/Gadget.h
@@ -0,0 +1,87 @@
+/*
+RealTimeBattle, a robot programming game for Unix
+Copyright (C) 1998-2000  Erik Ouchterlony and Ragnar Ouchterlony
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software Foundation,
+Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+*/
+
+#ifndef __GADGET__
+#define __GADGET__
+
+#include <string>
+#include <list>
+
+#include "GadgetSet.h"
+
+class Variable;
+class Function;
+
+struct VariableDefinition;
+struct FunctionDefinition;
+
+class Gadget
+{
+public:
+
+
+  Gadget() : 
+    info(this, 0, ""), 
+    variables(NULL), functions(NULL) {}
+
+  Gadget( const char* name, Gadget* const p ) : 
+    info(this, last_id_used++, name), 
+    parent(p), 
+    variables(NULL), functions(NULL) {}
+
+  ~Gadget();
+
+
+  long int get_unique_id() { return info.id; }
+  string& get_name() { return info.name; }
+
+  const GadgetInfo& get_info() { return info; }
+
+  Gadget* get_parent() { return parent; }
+  GadgetSet& get_my_gadgets() { return my_gadgets; }
+
+  static void set_last_id_used() { last_id_used = 0; }
+  
+  void eval_function(const int fcn) {}
+
+protected:
+
+  void init_variables( const VariableDefinition* var_def, const int last_var );
+  void init_functions( const FunctionDefinition* fcn_def, const int last_fcn );
+
+  GadgetInfo info;
+
+
+  Gadget* parent;
+
+  // Pointer to the gadget in the GadgetDefinition hierarchy which defined
+  // this gadget
+  //  Gadget* defining_gadget;
+
+  GadgetSet my_gadgets;
+
+  Variable* variables;
+  Function* functions;
+
+
+public:
+  static int last_id_used;
+};
+
+#endif __GADGET__
---
+++ src/Gadgets/WeaponGadget.cc
@@ -0,0 +1,108 @@
+/*
+RealTimeBattle, a robot programming game for Unix
+Copyright (C) 1998-2000  Erik Ouchterlony and Ragnar Ouchterlony
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software Foundation,
+Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+*/
+
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif
+
+#include "WeaponGadget.h"
+#include "Variable.h"
+
+
+const VariableDefinition 
+WeaponGadget::variable_def[WeaponGadget::LAST_WEAPONVAR] = 
+{
+  {"Ammunition", INT_V, 0, 0, INT_MAX,0,false,true,false},
+  
+  {"RotateToAngle", DOUBLE_V, 0, -DBL_MAX, DBL_MAX,0, false, true, true },
+  {"RotateAmountAngle", DOUBLE_V, 0, -DBL_MAX, DBL_MAX,0, false, true, true },
+  {"RotateSpeed", DOUBLE_V, 0, 0.0, DBL_MAX,0, false, true, true },
+  
+  {"AutoFire", BOOL_V, false,0,0,0,false,true,true}
+
+//      "MaxRotateSpeed",
+
+//      "SoundLevel",
+
+//      "MountTime",
+//      "UnmountTime",
+//      "MountCost",
+//      "UnmountCost",
+
+//      "UseCostPerTime",
+
+//      "AmmoLimit",
+//      "StartAmmo",
+//      "ReloadTime",
+
+//      "TemperaturePerDamage",
+//      "MaxTemperature",
+
+//      "CoolDownPerTime",
+
+//      "Attachable",
+
+//      "Colour",
+//      "Price",
+//      "Mass"
+};
+
+
+const FunctionDefinition 
+WeaponGadget::function_def[WeaponGadget::LAST_WEAPONFCN] = 
+{
+  { "Shoot", true },
+  { "Mount", true },
+  { "Unmount", true },
+  { "Drop", true },
+  { "Rotate", true },
+  { "RotateTo", true },
+  { "RotateAmount", true }
+};
+
+WeaponGadget::WeaponGadget( const char* name, Gadget* const p ) 
+  : Gadget(name, p)
+{
+  init_variables(variable_def, LAST_WEAPONVAR);
+  init_functions(function_def, LAST_WEAPONFCN);
+}
+
+void
+WeaponGadget::eval_function( const int fcn )
+{
+  
+  switch( WeaponFcns(fcn) )
+    {
+    case SHOOT:
+      shoot();
+      break;
+    case MOUNT:
+      break;
+    case UNMOUNT:
+      break;
+    case DROP:
+      break;
+    case ROTATE:
+      break;
+    case ROTATETO:
+      break;
+    case ROTATEAMOUNT:
+      break;
+    }
+}
