fabac4583b4d37a984059598e8dc5cd974ed904d
Erik Ouchterlony <ejlo@users.sourceforge.net>
2000-05-21 16:12:28 +0000

    ArenaGadget added

--- src/Gadgets/ArenaGadget.h
+++ src/Gadgets/ArenaGadget.h
@@ -17,21 +17,44 @@ along with this program; if not, write to the Free Software Foundation,
 Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
 */
 
-#ifndef __ALLGADGETS__
-#define __ALLGADGETS__
+#ifndef __ARENA_GADGET__
+#define __ARENA_GADGET__
 
+#include <limits.h>
+#include <float.h>
 
 #include "Gadget.h"
-#include "GadgetSet.h"
-
-#include "Function.h"
 #include "Variable.h"
-
-#include "ArenaGadget.h"
-#include "CommunicationGadget.h"
-#include "RobotBodyGadget.h"
-#include "ShotGadget.h"
-#include "WeaponGadget.h"
+#include "Function.h"
 
 
-#endif  __ALLGADGETS__
+class ArenaGadget : public Gadget
+{
+public:
+
+  ArenaGadget( const char* name, Gadget* const p );
+
+private:
+
+  enum ArenaVars
+  {
+     LAST_ARENAVAR
+  };
+
+  const static VariableDefinition variable_def[LAST_ARENAVAR]; 
+
+
+
+  enum ArenaFcns
+  {
+    LAST_ARENAFCN
+  };
+
+  const static FunctionDefinition function_def[LAST_ARENAFCN];
+  
+  void eval_function(const int fcn);
+
+};
+
+
+#endif __ARENA_GADGET__
