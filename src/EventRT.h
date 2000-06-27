f0a56c71cd54bd55c376f923b39fae5e662ea9d8
Benoit Rousseau <roussebe@users.sourceforge.net>
2000-06-27 14:57:18 +0000

    Some more RealTime Events

---
+++ src/EventRT.h
@@ -0,0 +1,57 @@
+/*
+RealTimeBattle, a robot programming game for Unix
+Copyright (C) 1998-2000  Erik Ouchterlony and Ragnar Ouchterlony
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software Foundation,
+Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+*/
+
+#ifndef __EventRT__
+#define __EventRT__
+
+#include "Event.h"
+
+class CheckGUIEvent : public Event()
+{
+  CheckGUIEvent();
+  //This is your go Ragnar :)
+};
+
+
+
+
+class PrepareForNewMatchEvent : public Event
+{
+ public:
+  PrepareForNewMatchEvent( const double time, Tournament* t) 
+    : Event(time), my_tournament(t) {}
+};
+
+class StartNewMatchEvent : public Event
+{
+public:
+  StartNewMatchEvent( const double time, Match* m) 
+    : Event(time), my_match(m) {}
+};
+
+class EndMatchEvent : public Event
+{
+public:
+  EndMatchEvent( const double time, Match* m) 
+    : Event(time), my_match(m) {}
+};
+
+
+
+#endif
