3ec7849a4b3ce9bb08a0db5e12d1de1822f4d609
Benoit Rousseau <roussebe@users.sourceforge.net>
2000-10-31 21:14:28 +0000

    Just a new way to destroy the Gadgets' vector...

--- utils/MapEditor/src/Arena.cc
+++ utils/MapEditor/src/Arena.cc
@@ -1,22 +1,28 @@
 #include <iostream.h>
 #include "Arena.h"
 #include "Gadget.h"
+#include <algorithm>
+
+using namespace std;
 
 void Arena::Write()
-{
-  for(vector<Gadget*>::iterator i = TheGadgets.begin(); i != TheGadgets.end(); i++)
+{  
+  for(vector<Gadget*>::iterator i=TheGadgets.begin(); i!=TheGadgets.end(); i++)
     {
       (*i)->Print();
     }
 }
 
+template<class T> struct deallocator    
+//To dealloc the memory used by pointers in a vector...
+{
+  void operator()(T const p) {delete p;}
+};
 
 Arena::~Arena()
 {
-  for(vector<Gadget*>::iterator i = TheGadgets.begin(); i != TheGadgets.end(); i++)
-    {
-      delete (*i);
-    }
+  for_each(TheGadgets.begin(), TheGadgets.end(),deallocator<Gadget*>());
+  TheGadgets.clear();
 }
 
 
