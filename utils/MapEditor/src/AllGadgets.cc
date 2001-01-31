04b2a30d380188b3b67e207a79f799ee0ba01a1a
Benoit Rousseau <roussebe@users.sourceforge.net>
2001-01-31 15:44:01 +0000

    Forgiven files

--- utils/MapEditor/src/AllGadgets.cc
+++ utils/MapEditor/src/AllGadgets.cc
@@ -3,10 +3,13 @@
 
 BasicGadget AllGadgets[] = 
 {
-  {"Weapon",    new WeaponGadget("", NULL)}, 
-  {"Shot",      new ShotGadget("", NULL)}, 
-  {"Explosion", new ExplosionGadget("", NULL)},
-  {"Wall",      new WallGadget("", NULL)},
+  {"Weapon",      new WeaponGadget("", NULL)}, 
+  {"Shot",        new ShotGadget("", NULL)}, 
+  {"Explosion",   new ExplosionGadget("", NULL)},
+  {"Wall",        new WallGadget("", NULL)},
+  {"Defense",     new DefenseGadget("", NULL)},
+  {"Sensor",      new SensorGadget("", NULL)},
+  {"Environment", new EnvironmentGadget("", NULL)},
   {"", 0}
 };
 
