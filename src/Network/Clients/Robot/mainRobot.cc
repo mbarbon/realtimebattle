9d3b87430ab77b3499e20e064f5d152738d93c4c
Benoit Rousseau <roussebe@users.sourceforge.net>
2001-08-01 10:21:38 +0000

    Can be run from a chat_client now we need to run the robot and to make it comunicate with the server

--- src/Network/Clients/Robot/mainRobot.cc
+++ src/Network/Clients/Robot/mainRobot.cc
@@ -1,28 +1,67 @@
+/*
+RealTimeBattle, a robot programming game for Unix
+Copyright (C) 1998-2001  Erik Ouchterlony and Ragnar Ouchterlony
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software Foundation,
+Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+*/
+
+
 #include "ClientSocket.h"
 #include <signal.h>
 
+#define WEBSITE_URL      "http://"
 
 int
 main( int argc, char* argv[] )
 {
-  string host_name;
-  int PortNum;
+  /*
+    argv[0] : rtbrobot
+    argv[1] : robot path
+    argv[2] : server address
+    argv[3] : server port
+  */
 
-  while(1)
+  /* 
+     string host_name;
+     int PortNum;
+  */
+
+  if( argc != 4 )
     {
-      cout<<"ServerName : ";
-      cin>>host_name;
-      cout<<"Port Number : ";
-      cin>>PortNum;
-      my_socketclient->connect_to_server( host_name, PortNum );
-      string name;
-      cout<<"Name : ";
-      cin>>name;
-      my_socketclient->initialize( name, ROBOT_CLIENT_CONNECTION );
-      while( my_socketclient->is_connected() )
-	my_socketclient->check_connection();
-
-      my_socketclient->close_connection();
+      cerr<<"rtbrobot : This program is used by RealTimeBattle to establish a comunication\n"
+	  <<"           between a robot and the RealTimeBattle server.\n"
+	  <<"           You should use this program by yourself.\n\n";
+      cout<<"You can learn more about RealTimeBattle at "<<WEBSITE_URL<<endl;
+      exit( 0 );
     }
-  return 0;
+
+  
+  cout<<"[rtbrobot "<<argv[1]<<" "<<argv[2]<<" "<<argv[3]<<"]"<<endl;
+  /*
+    cin>>host_name;
+    cout<<"Port Number : ";
+    cin>>PortNum;
+    my_socketclient->connect_to_server( host_name, PortNum );
+    string name;
+    cout<<"Name : ";
+    cin>>name;
+    my_socketclient->initialize( name, ROBOT_CLIENT_CONNECTION );
+    while( my_socketclient->is_connected() )
+    my_socketclient->check_connection();
+    
+    my_socketclient->close_connection();
+  */
+  exit( 0 );
 }
