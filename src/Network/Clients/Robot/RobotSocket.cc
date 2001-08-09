01104a10663b73fadfae8f37598bb66fd84ef9c0
Benoit Rousseau <roussebe@users.sourceforge.net>
2001-08-09 17:12:21 +0000

    Get the messages from the robot and send them to the server and vice versa TODO : make it more friendly for futur modifications :-)

--- src/Network/Clients/Robot/RobotSocket.cc
+++ src/Network/Clients/Robot/RobotSocket.cc
@@ -25,29 +25,52 @@ Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
 #include <netdb.h>
 #include <netinet/in.h>
 #include <sys/socket.h>
+#include <sys/time.h>
 #include <unistd.h>
 
 #include "RobotPackets.h"
-#include "ClientSocket.h"
+#include "RobotSocket.h"
+
+void 
+SocketRobot::initialize( const string& auth_key )
+{
+  SocketClient::initialize( auth_key, ROBOT_CLIENT_CONNECTION ); //Say to the server who I am
+  P.start( NORMAL_MODE );   //Start the robot (maybe we should wait for a reply from server first...
+}
 
 void
-SocketClient::check_connection()
+SocketRobot::check_connection()
 {
+  struct timeval tv;
+  tv.tv_sec  = 0;
+  tv.tv_usec = 500000;
+
   fd_set readfs;
   fd_set exceptfs;
 
   FD_ZERO( &readfs );
   FD_ZERO( &exceptfs );
-
+  
   FD_SET( nc.the_socket, &readfs );
   FD_SET( nc.the_socket, &exceptfs );
-  FD_SET( 0 , &readfs ); 
   
-  my_cons.prompt_off();
-
-  if( select( nc.the_socket + 1, &readfs, NULL, &exceptfs, NULL ) < 0 )
+  while( ! P.get_instreamp()->eof() )
     {
-      my_cons.write(C_FAIL,  "select failed." );
+      /*
+       * TODO : I can't remember the name of the defined var for max_buf_length... sorry
+       */
+      
+      char temp[512] ;
+      P.get_instreamp()->getline( temp, 512, '\n' );
+      
+      string tmp_str(temp);
+
+      nc.send_data( RobotMessagePacket( tmp_str ).make_netstring() );
+    }
+
+  if( select( nc.the_socket + 1, &readfs, NULL, &exceptfs, &tv ) < 0 )
+    {
+      cerr<<"Select failed in SocketRobot::check_connection\n";
       exit(1);
     }
 
@@ -56,23 +79,7 @@ SocketClient::check_connection()
       my_cons.write(C_OK, "Exception for client." );
       nc.close_socket();
     }
-
-  if( FD_ISSET( 0, &readfs ) )
-    {
-      char buffer[256];
-      bzero(buffer, 256);
-      fgets(buffer, 255, stdin);
-      my_cons.prompt_enter();
-      my_cons.update_prompt();
-      string buf = string (buffer);
-      if( buf == "quit")   //The quit event (maybe a click for a chat)
-	{ 
-	  //Use ClientRequest packet instead ...
-	  nc.send_data( CommandPacket("Quit").make_netstring() );
-	  exit( 0 );
-	}
-    }
-
+  
   if( (nc).connected && FD_ISSET( (nc).the_socket, &readfs ) )
     {
       int read = nc.read_data();
@@ -107,51 +114,6 @@ SocketClient::check_connection()
 	  (nc).close_socket();
 	}
     }
-  my_cons.prompt_on();
-}
-
-void 
-SocketClient::ask_for_info_to_metaserver()
-{
-
-}
-
-
-
-void
-SocketClient::initialize( const string& name, const client_t& c )
-{
-//    string str = (string)( (char)(RTB_NETPROTOCOL_V1 & 0xff) ) + name;
-
-//    int len = str.length();
-//    string send_string = (string)( (char)(PACKET_INIT & 0xff) )
-
-  InitializationPacket IP(  RTB_NETPROTOCOL_V1, 
-			    c, name );
-  nc.name = name;
-  nc.send_data( IP.make_netstring() );
-}
-
-void
-SocketClient::close_connection()
-{
-  close( nc.the_socket );
-  nc.connected = false;
-  nc.the_socket = -1;
-}
-
-bool
-SocketClient::is_connected()
-{
-  return !nc.is_not_connected();
-}
-
-SocketClient* my_socketclient = new SocketClient;
-
-void
-exit_cleanly(int Sign)
-{
-  quit();
 }
 
 void
